92c530cd47502adab685beeb9aec58799b127f17
David González Barrera <david.gonzalez@treelogic.com>
2020-03-25 17:00:07 +0100

    improve scritps and add information to readme

--- use_case_pipeline_distriburted_inference/script/inference-workflow.sh
+++ use_case_pipeline_distriburted_inference/script/inference-workflow.sh
@@ -19,7 +19,7 @@ cd ..
 # Split files
 echo ""
 echo "Kubernetes: "
-cat 02-dh-job-splityaml.yaml | sed "s|\$PARTNUMBER|${partnumber}|g" | sed "s|\$PODINPUT|${pinput}|g" | sed "s|\$PODOUTPUT|${poutput}|g" | kubectl apply -f -
+cat 03-dh-job-splityaml.yaml | sed "s|\$PARTNUMBER|${partnumber}|g" | sed "s|\$PODINPUT|${pinput}|g" | sed "s|\$PODOUTPUT|${poutput}|g" | kubectl apply -f -
 
 COND="Not Complete"
 
@@ -39,14 +39,10 @@ prefix='part'
 for i in $(seq -f "%03g" 0 $partsubtractone)
 do 
    name=(part-$i-$originalfilename)
-   cat 03-dh-job-inference.yaml | sed "s/\$PART/${part-$i}/" | sed "s/\$FILENAME/${name}/" | sed "s/\$NUMBER/${parnumber}/" | kubectl apply -f - | printf "" 
+   cat 04-dh-job-inference.yaml | sed "s/\$PART/${part-$i}/" | sed "s/\$FILENAME/${name}/" | sed "s/\$NUMBER/${parnumber}/" | kubectl apply -f - | printf "" 
 done
 echo "Done!"
 
 echo ""
 echo "Worflows running!"
-echo ""
-echo "Use [kubectl get job -n dh] to check the status of the jobs."
-echo "Use [kubectl get pod -n dh] to check the status of the  for each pod by job."
-echo "Use [kubectl logs "POD_NAME" -n dh] to check the status of the logs for each job."
 echo ""
